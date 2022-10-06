08f11e7dd244dec53e5d437fc67e99c17c50931e
Maykeul <michael.mittoo@gmail.com>
2022-10-06 11:58:34 +0200

    darkmode done

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-const darkModeElement = document.querySelector(".navbar__darkmode");
+const darkModeElement = document.querySelector(".navbar__darkmodeImg");
+const anecdoteTextElement = document.querySelector(".anecdote-content__text");
+darkModeElement.style.width = "32px";
+
 darkModeElement.addEventListener("click", () => {
-    document.body.style.backgroundColor = "black";
+    document.body.classList.toggle("dark-background");
+    console.log(anecdoteTextElement);
+    anecdoteTextElement.classList.toggle("dark-font");
+    console.log(darkModeElement);
+    if (darkModeElement.src == "http://127.0.0.1:5500/darkmode.png") {
+        console.log(darkModeElement.src);
+        darkModeElement.src = "lightmode.png";
+        console.log(darkModeElement.src);
+    } else {
+        darkModeElement.src = "darkmode.png";
+    }
 });
